18fbab19d50b88a6e4a96d3db7ddde718e008486
SerhiiSterniichuk <sterniichuk.serhii@gmail.com>
2023-01-03 17:25:01 +0200

    social list and header in login is done

---
+++ asserts/js/log-in.js
@@ -0,0 +1,57 @@
+const signUpClassName = "sign-up-is-selected";
+const logInClassName = "log-in-is-selected";
+const noneState = "none";
+const initialState = "initial";
+const nonActiveColorText = '#949AB2';
+const activeColorText = '#292D3C';
+const headlineOfSignUp = document.querySelector('#headline-of-sign-up-switch');
+const headlineOfLogIn = document.querySelector('#headline-of-log-in-switch');
+
+
+function signUpIsSelected() {
+    toggle(signUpClassName, initialState);
+    toggle(logInClassName, noneState);
+    toggleOpacityById("img-underline-log-in", 0);
+    toggleOpacityById("img-underline-sign-up", 1);
+    headlineSignUpOn();
+    headlineLogInUpOff();
+}
+
+function logInIsSelected() {
+    toggle(signUpClassName, noneState);
+    toggle(logInClassName, initialState);
+    toggleOpacityById("img-underline-log-in", 1);
+    toggleOpacityById("img-underline-sign-up", 0);
+    headlineSignUpOff();
+    headlineLogInUpOn();
+}
+
+function toggleOpacityById(idName, value) {
+    var element = document.getElementById(idName);
+    element.style.opacity = value;
+}
+
+
+function headlineSignUpOn() {
+    headlineOfSignUp.style.color = activeColorText;
+}
+
+function headlineSignUpOff() {
+    headlineOfSignUp.style.color = nonActiveColorText;
+}
+
+function headlineLogInUpOn() {
+    headlineOfLogIn.style.color = activeColorText;
+}
+
+function headlineLogInUpOff() {
+    headlineOfLogIn.style.color = nonActiveColorText;
+}
+
+function toggle(className, displayState) {
+    var elements = document.getElementsByClassName(className)
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = displayState;
+    }
+}
